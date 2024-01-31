f38fd7c2c0594058a3c302896f3ee9162a70807b
MrFlashed <53785391+MrFlashed@users.noreply.github.com>
2024-01-31 14:30:05 +0100

    Added footer to securitytest.php + footer fix

--- securitytest.php
+++ securitytest.php
@@ -95,8 +95,16 @@
 
     </main>
 
-    <footer>
-        &copy; 2024 Your Website Name. All rights reserved.
+    <footer class="footer">
+    <!-- Additional Elements -->
+    <div class="footer-icons">
+        <a href="#" target="_blank">Facebook</a>
+        <a href="#" target="_blank">Twitter</a>
+        <a href="#" target="_blank">Instagram</a>
+        <!-- Add more social media icons as needed -->
+    </div>
+
+    <p>&copy; 2024 Your Website Name. All rights reserved. | Designed by <a href="http://localhost/SecurityChief/#" target="_blank">Security Design Team</a></p>
     </footer>
 
 </body>
